b121010b837b58e7a071dd30c3b7667382082ae6
leslie yip <leslieyip02@gmail.com>
2023-11-29 10:46:09 +0800

    Updated 29/11/2023

---
+++ completed/0191.java
@@ -0,0 +1,20 @@
+class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int c = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) == 1) {
+                c++;
+            }
+            n >>= 1;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int n = -3;
+
+        Solution solution = new Solution();
+        System.out.println(solution.hammingWeight(n));
+    }
+}
