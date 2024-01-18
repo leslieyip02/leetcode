ff291798f3e99f83f6cfc9d953ab19592d75ae9d
leslie yip <leslieyip02@gmail.com>
2024-01-18 11:00:42 +0800

    Updated 18/01/2024

--- completed/0070.java
+++ completed/0070.java
@@ -1,28 +1,15 @@
 class Solution {
-    private int[] memo;
-
-    private int climb(int n) {
-        if (memo[n] == 0) {
-            memo[n] = climb(n - 1) + climb(n - 2);
-        }
-        return memo[n];
-    }
-
     public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        if (n == 1 || n == 2) {
+            return n;
         }
 
-        this.memo = new int[n + 1];
-        this.memo[1] = 1;
-        this.memo[2] = 2;
-        return climb(n);
-    }
-
-    public static void main(String[] args) {
-        int n = 4;
-
-        Solution solution = new Solution();
-        System.out.println(solution.climbStairs(n));
+        int[] ways = new int[n];
+        ways[0] = 1;
+        ways[1] = 2;
+        for (int i = 2; i < n; i++) {
+            ways[i] = ways[i - 1] + ways[i - 2];
+        }
+        return ways[n - 1];
     }
 }
