ccb21412f4ef3ae4b98e264778317851f2c5816d
leslie yip <leslieyip02@gmail.com>
2024-04-16 10:39:42 +0800

    Updated 16/04/2024

---
+++ completed/0623.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return root;
+        } else if (depth == 1){
+            TreeNode newRoot = new TreeNode(val, root, null);
+            return newRoot;
+        } else if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+            return root;
+        } else {
+            addOneRow(root.left, val, depth - 1);
+            addOneRow(root.right, val, depth - 1);
+            return root;
+        }
+    }
+}
