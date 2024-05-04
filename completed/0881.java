63f09b157dfe3f369b018b5820167b0e2ba2e985
leslie yip <leslieyip02@gmail.com>
2024-05-04 13:24:36 +0800

    Updated 04/05/2024

---
+++ completed/0881.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+        while (left <= right) {
+            if (people[right] + people[left] <= limit) {
+                left++;
+            }
+            right--;
+            boats++;
+        }
+        return boats;
+    }
+}
