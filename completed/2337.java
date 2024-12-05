f6eb5671f382f7a633bdded876623964738aef22
leslie yip <leslieyip02@gmail.com>
2024-12-05 09:27:31 +0800

    Updated 05/12/2024

--- completed/2337.java
+++ completed/2337.java
@@ -2,65 +2,29 @@ import java.util.*;
 
 class Solution {
     public boolean canChange(String start, String target) {
-        int n = start.length();
-        char[] positions = start.toCharArray();
-
-        for (int i = 0; i < n; i++) {
-            if (positions[i] == target.charAt(i)) {
-                continue;
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < target.length(); i++) {
+            if (target.charAt(i) == 'L') {
+                if (r > 0) {
+                    return false;
+                }
+                l++;
+            } else if (target.charAt(i) == 'R') {
+                r--;
             }
-
-            if (target.charAt(i) == '_') {
-                if (positions[i] == 'L') {
-                    // cannot go left since everything on the left is correct
+            if (start.charAt(i) == 'R') {
+                if (l > 0) {
                     return false;
-                } else {
-                    // shift everything right to make space
-                    int j = i;
-                    boolean canShift = false;
-                    for (; j < n - 1; j++) {
-                        if (positions[j] == 'R' && positions[j + 1] == '_') {
-                            canShift = true;
-                            break;
-                        }
-                    }
-
-                    if (!canShift) {
-                        return false;
-                    } else {
-                        for (int k = j + 1; k > i; k--) {
-                            positions[k] = positions[k - 1];
-                        }
-                        positions[i] = '_';
-                    }
                 }
-            } else if (target.charAt(i) == 'L') {
-                if (positions[i] == 'R') {
-                    // cannot move an L into this position
-                    return false;
-                } else {
-                    int j = i + 1;
-                    boolean foundL = false;
-                    for (; j < n; j++) {
-                        if (positions[j] == 'L') {
-                            foundL = true;
-                            break;
-                        } else if (positions[j] == 'R') {
-                            return false;
-                        }
-                    }
-                    if (!foundL) {
-                        return false;
-                    } else {
-                        positions[i] = 'L';
-                        positions[j] = '_';
-                    }
-                }
-            } else {
-                // not possible to get a R from the left
+                r++;
+            } else if (start.charAt(i) == 'L') {
+                l--;
+            }
+            if (l < 0 || r < 0) {
                 return false;
             }
         }
-        return true;
+        return l == 0 && r == 0;
     }
 }
