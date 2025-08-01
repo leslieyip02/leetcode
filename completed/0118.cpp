952480d0f94154736108820918cec9c57faf8f71
leslie yip <leslieyip02@gmail.com>
2025-08-01 11:01:54 +0800

    Updated 01/08/2025

--- completed/0118.cpp
+++ completed/0118.cpp
@@ -1,36 +1,19 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        if (numRows == 1) {
-            return {{ 1 }};
+        vector<vector<int>> triangle;
+        triangle.push_back({ 1 });
+
+        for (int i = 1; i < numRows; i++) {
+            vector<int> row;
+            row.push_back(1);
+            for (int j = 0; j < triangle[i - 1].size() - 1; j++) {
+                row.push_back(triangle[i - 1][j] + triangle[i - 1][j + 1]);
+            }
+            row.push_back(1);
+            triangle.push_back(row);
         }
 
-        auto rows = generate(numRows - 1);
-        vector<int> row = { 1 };
-        for (int i = 0; i < numRows - 2; i++) {
-            row.push_back(rows.back()[i] + rows.back()[i + 1]);
-        }
-        row.push_back(1);
-        rows.push_back(row);
-        return rows;
+        return triangle;
     }
 };
-
-int main() {
-    // int numRows = 5;
-    int numRows = 30;
-
-    Solution solution;
-    auto results = solution.generate(numRows);
-    for (auto row : results) {
-        for (int i : row) {
-            cout << i << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
